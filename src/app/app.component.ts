a2a7e2e55643f9306b3f63df47001d339a70e8d3
Fredy Sarmiento <ing.fredysarmiento@gmail.com>
2021-10-11 15:59:52 -0500

    ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,23 @@ export class AppComponent {
   age = 28;
   img = "https://www.w3schools.com/howto/img_avatar.png"
   btnDisabled=true;
+  person={
+    name:'fredycode',
+    age: 28,
+    avatar:'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  toggleButton(){
+    this.btnDisabled=!this.btnDisabled;
+  }
+  increaseAge(){
+    this.person.age +=1;
+  }
+  onScroll(event:Event){
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop)
+  }
+  changeName(event:Event){
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
